cdb5ff1480764283c1fcfaae5596aeda86944d0c
qingxiangchayun <yuanli891212@163.com>
2015-01-06 19:03:27 +0800

    pubsub

---
+++ pub-sub.js
@@ -0,0 +1,35 @@
+/**
+ * PubSub 实现
+ */
+
+(function(window){
+	'use strict';
+
+	var PubSub = {
+		handlers : {}
+	};
+
+	PubSub.on = function(eventType, handler){
+
+		if( !(eventType in this.handlers) ){
+			this.handlers[eventType] = [];
+		}
+		
+		this.handlers[eventType].push(handler);
+
+		return this;
+	};
+
+	PubSub.emit = function(eventType){
+		var handlerArgs = Array.prototype.slice.call(arguments,1);
+
+		for(var i=0,len=this.handlers[eventType].length; i<len; i++){
+			this.handlers[eventType][i].apply(this,handlerArgs);
+		}
+
+		return this;
+	};
+
+	window.PubSub = PubSub;
+
+})(window);
