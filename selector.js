31cfdec4ded68ecd43c6d0615ac3f75b3cbf96b0
qingxiangchayun <yuanli891212@163.com>
2015-01-31 11:44:04 +0800

    selector v0.1 fix

--- selector.js
+++ selector.js
@@ -23,39 +23,73 @@
 		}else{
 			return str.replace(/^\s+|\s+$/g,'');
 		}
+	};
+
+	// elem in array
+	var inArray = function(elem,arr){
+		for(var i=0,len=arr.length; i<len; i++){
+			if(elem === arr[i]){
+				return true;
+			}
+		}
+
+		return false;
+	};
+
+	// array in array  
+	// [a,b] in [a,b,c] true
+	// [a,b,d] in [a,b,c] false
+	var arrInArray = function(partArr,allArr){
+		var result = true;
+
+		for(var i=0,len=partArr.length; i<len; i++){
+			if( !inArray( partArr[i], allArr ) ){
+				result = false;
+				break;
+			}
+		}
+
+		return result;
 	}
 
-	var getByClassName = function(className){
+	// classNameArr [classA] [classA,classB]
+	var getByClassName = function(classNameArr){
 		var match = [];
 		
 		// HTMLCollection
 		var list;
 
-		var currentClass;
-		var classArr = [];
+		var currentClassArr ;
 
 		// 浏览器支持 getElementsByClassName
-		/*if(document.getElementsByClassName){
-			return document.getElementsByClassName(className);
-		}else{*/
+		if(document.getElementsByClassName){
+			return document.getElementsByClassName(classNameArr.join(' '));
+		}else{
 			list = document.getElementsByTagName('*');
 
 			for(var i=0,len=list.length; i<len; i++){
+				
 				// 将多个空格替换成一个空格  <div class="list   active">  'list   active' --> 'list active'
-				rSpace.lastIndex = 0;
-				currentClass = list[i].className.replace(rSpace,' ');
+				// rSpace.lastIndex = 0;
+				// currentClass = list[i].className.replace(rSpace,' ');
 				
 				// test 方式不能匹配这种情况  传入className为 '.classA.classC'  html的class为 ' classA classB classC'
-				/*if( new RegExp('\\b'+className + '\\b','g').test( currentClass ) ){
-					match.push(list[i]);
-				}*/
+				// if( new RegExp('\\b'+className + '\\b','g').test( currentClass ) ){
+				// 	match.push(list[i]);
+				// }
 
-				classArr = toolStrim( list[i].className ).split(/\s+/);
+				
+				// ' classA classB classC ' --> [classA,classB,classC]
+				currentClassArr = toolStrim( list[i].className ).split(/\s+/);
+
+				if( arrInArray( classNameArr, currentClassArr ) ){
+					match.push( list[i] );
+				}
 
 			}
 
 			return match;
-		//}
+		}
 	};
 
 	/**
@@ -64,7 +98,7 @@
 	 * @return {[type]}          [description]
 	 */
 	var selector = function(str){
-		var className;
+		var classNameArr;
 
 		if(!str){
 			throw new TypeError('arguments error');
@@ -74,14 +108,14 @@
 		if(rId.test(str)){
 			return document.getElementById( str.substring(1) );
 
-		// '.class' '.classA.classB' '.classA .classB'
+		// '.class  ' '.classA.classB' '.classA .classB'
 		}else if(rClass.test(str)){
-
-			// 1、. 替换为 空格 .classA.classB --> classA classB
-			// 2、删除前后空格且将  
+			// trim '.classA   ' --> '.classA'
+			// substring '.classA' --> 'classA'
+			// split '.classA.classB' --> [classA,classB]
 			
-			className = toolStrim( str.replace(/\./g,' ') );
-			return getByClassName(className);
+			classNameArr = toolStrim(str).substring(1).split('.')
+			return getByClassName(classNameArr);
 		}
 
 	};
